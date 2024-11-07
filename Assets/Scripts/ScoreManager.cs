725393185725604d21e1ef5a7af116fc82ae62d7
Eduardo2506 <eduardocastro250612@gmail.com>
2024-11-06 20:14:33 -0500

    Creacion de interfaz del juego

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("TEXTS")]
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI timeText;
+
+    [Header("SETTINGS")]
+    [SerializeField] private float tiem;
+
+
+}
